bef741560a36ae9a0737680d657a88e6d35e5d9f
medetisrafilov <israfilovmedet52@gmail.com>
2024-11-06 16:42:24 -1200

    yeni ders

---
+++ abb.js
@@ -0,0 +1,60 @@
+let a = promt("Vaxt Teyin Edin Meselen: 2024.05.08");
+
+
+Number(b[0])
+Number(b[1])
+Number(b[2])
+if (b[1] == 1)  {
+    console.log (b[3] + " " + "Yanvar" + " " + b[0] + "cu il")
+
+}
+else if (b[1] == 2) {
+    console.log (b[3] + " " + "fevral" + " " + b[0] + "cu il")
+
+
+}
+else if (b[1] == 3) {
+    console.log (b[3] + " " + "Mart" + " " + b[0] + "cu il")
+
+}
+else if (b[1] == 4) {
+    console.log (b[3] + " " + "aprel" + " " + b[0] + "cu il")
+
+}
+else if (b[1] == 5) {
+    console.log (b[3] + " " + "may" + " " + b[0] + "cu il")
+
+}
+else if (b[1] == 6) {
+    console.log (b[3] + " " + "iyun" + " " + b[0] + "cu il")
+
+}
+else if (b[1] == 7) {
+    console.log (b[3] + " " + "iyul" + " " + b[0] + "cu il")
+
+}
+else if (b[1] == 8) {
+    console.log (b[3] + " " + "avustos" + " " + b[0] + "cu il")
+
+}
+else if (b[1] == 9) {
+    console.log (b[3] + " " + "sentyabir" + " " + b[0] + "cu il")
+
+}
+else if (b[1] == 10) {
+    console.log (b[3] + " " + "Oktyabir" + " " + b[0] + "cu il")
+
+}
+else if (b[1] == 11) {
+    console.log (b[3] + " " + "Noyabir" + " " + b[0] + "cu il")
+
+}
+else if (b[1] == 12) {
+    console.log (b[3] + " " + "dekambir" + " " + b[0] + "cu il")
+
+}
+else (b[1]>12)
+    { alert("Netice Tapilmad yalnis Emeliyat")
+
+    }
+
